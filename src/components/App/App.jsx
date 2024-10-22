43b001668d0321403de2d4e7dea1b4be27e5344e
frasdor <frasunkiewicz.dorota@gmail.com>
2024-10-22 10:54:54 +0200

    added hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,48 +1,50 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import FeedbackOptions from '../FeedbackOptions/FeedbackOptions';
 import Statistics from '../Statistics/Statistics';
 import Section from '../Section/Section';
 import Notification from '../Notification/Notification';
 import styles from './App.module.css'; 
 
-class App extends Component {
-  state = {
+const App = () => {
+ const [ feedback, setFeedback ] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
-  };
+ });
+
 
   // Metoda obsługująca kliknięcie przycisku i aktualizująca stan
-  handleFeedback = (option) => {
-    this.setState((prevState) => ({
-      [option]: prevState[option] + 1,
+  const handleFeedback = (option) => {
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [option]: prevFeedback[option] + 1,
     }));
   };
 
   // Metoda licząca łączną liczbę opinii
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  const countTotalFeedback = () => {
+    const { good, neutral, bad } = feedback;
     return good + neutral + bad;
   };
 
   // Metoda obliczająca procent pozytywnych opinii
-  countPositiveFeedbackPercentage = () => {
-    const total = this.countTotalFeedback();
-    const { good } = this.state;
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
+    const { good } = feedback;
     return total ? Math.round((good / total) * 100) : 0;
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const positivePercentage = this.countPositiveFeedbackPercentage();
+ 
+    const { good, neutral, bad } = feedback;
+    const total = countTotalFeedback();
+    const positivePercentage = countPositiveFeedbackPercentage();
 
     return (
       <div className={styles.appContainer}>
         <Section title="Please leave feedback">
           <FeedbackOptions
             options={['good', 'neutral', 'bad']}
-            onLeaveFeedback={this.handleFeedback}
+            onLeaveFeedback={handleFeedback}
           />
         </Section>
 
@@ -61,7 +63,7 @@ class App extends Component {
         </Section>
       </div>
     );
-  }
-}
+  };
+
 
 export default App;
